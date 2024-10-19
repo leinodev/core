1db002608689b0dc46a8842de287c5ec4e94e0d6
Nikita <devkinitaleino@gmail.com>
2024-10-19 21:42:18 +0500

    - intermediate commit

--- config/db.go
+++ config/db.go
@@ -8,8 +8,17 @@ type (
 		Schemas    []Schema   `json:"schemas"`
 	}
 	Connection struct {
-		Dialect string `json:"dialect"`
+		Dialect  string              `json:"dialect"`
+		Postgres *PostgresConnection `json:"postgres" yaml:"postgres"`
 		// TODO: add configuration for connection according to selected dialect
+	}
+	PostgresConnection struct {
+		Host     string `yaml:"host" env:"POSTGRES_HOST"`
+		Port     string `yaml:"port" env:"POSTGRES_PORT"`
+		User     string `yaml:"user" env:"POSTGRES_USER"`
+		DBName   string `yaml:"dbName" env:"POSTGRES_DBNAME"`
+		SSLMode  bool   `yaml:"sslMode" env:"SSL_MODE"`
+		Password string `yaml:"password" env:"POSTGRES_PASSWORD"`
 	}
 	Schema struct {
 		Name    string      `json:"name"`
@@ -17,3 +26,13 @@ type (
 		Indexes []db.Index  `json:"indexes"`
 	}
 )
+
+func (cfg DBConfig) GetConnectionString() string {
+	switch cfg.Connection.Dialect {
+	case "postgres":
+		// TODO: build connection string for Postgres
+		return ""
+	default:
+		return ""
+	}
+}
